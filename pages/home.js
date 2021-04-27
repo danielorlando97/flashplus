773759876a613e02bc5a059c06a24d840c38b113
dany <dany@DESKTOP-60N0TVV.localdomain>
2021-04-27 10:26:45 -0700

    test

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,5 @@
 import { FaEllipsisV } from "react-icons/fa"
-import { BsArrow90DegLeft } from "react-icons/bs"
+import { GoThreeBars } from "react-icons/go"
 import { RiMenuAddFill } from "react-icons/ri"
 import { RiHomeGearLine } from "react-icons/ri"
 import { BiGridAlt } from "react-icons/bi"
@@ -13,21 +13,19 @@ function Home () {
 
   return(
     <div className="relative h-screen w-screen overflow-scroll">
-      <CircleContainerScaffolding className="fixed bg-orange bottom-2 right-2 z-30" dimension="h-16 w-16">
-        <AiOutlineSearch className="text-gray-100 text-opacity-50 text-3xl"/>
+      <CircleContainerScaffolding className="fixed bg-orange bottom-4 right-2 z-30" dimension="h-16 w-16">
+        <GoThreeBars className="text-gray-100 text-opacity-50 text-3xl"/>
       </CircleContainerScaffolding>
       <div className="fixed z-20 bg-body" >
         <div className="flex h-16 w-screen flex-col justify-center bg-back">
           <SimpleTopBarScaffolding className="pl-4 pr-2"
             componentName={<h1 className="text-orange text-3xl">PaqtTV+</h1>}>             
             <AiOutlineSearch className="text-gray-100 text-opacity-50 text-3xl"/>
-            <RiHomeGearLine className="text-gray-100 text-opacity-50 text-3xl"/>     
           </SimpleTopBarScaffolding>
         </div>
         <SimpleTopBarScaffolding height="h-8" className="px-3"
           componentName={<NavigateLabel/>}>
           <BiGridAlt className="mr-2 stroke-current stroke-0 text-gray-100 text-opacity-50 text-lg"/>
-          <FaEllipsisV className="stroke-current stroke-0 text-gray-100 text-opacity-50 text-lg"/>
         </SimpleTopBarScaffolding>
       </div>
       <div className="h-24 mb-2 w-screen"></div>
